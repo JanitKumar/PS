d10498bc1be1e649b055675c5815a330ad034802
Janit Kumar <janitkumar2041@gmail.com>
2026-05-19 20:47:25 +0530

    Add files via upload

---
+++ shape.java
@@ -0,0 +1,35 @@
+class circle{
+    static final double pi=3.14;
+    int radius;
+    public void setRadius(int radius){
+        this.radius=radius;
+    }
+    public double Area(){
+        return pi*radius*radius;
+    }
+
+}
+
+class cylinder extends circle{
+    int height;
+    public void setHeight(int height){
+        this.height=height;
+    }
+    @Override
+    public double Area(){
+          return 2*pi*radius*(radius*height);
+    }
+    
+}
+
+
+
+
+public class shape {
+  public static void main(String[] args) {
+    cylinder c1=new cylinder();
+    c1.setHeight(22);
+    c1.setRadius(7);
+    System.out.println(c1.Area());
+  }
+}
